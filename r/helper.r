f4d1ee6afce70f5904b7f2d71980a2d76ccba43f
Olson <andrew.olson@dfg.alaska.local>
2019-04-09 08:30:09 -0800

    Updated with more libraries and edits to text

---
+++ r/helper.r
@@ -0,0 +1,20 @@
+
+# libraries
+library(tidyverse)
+library(lubridate)
+library(FNGr)
+library(Distance)
+library(zoo)
+library(ggthemes)
+library(gridExtra)
+library(extrafont)
+library(tidyr)
+library(padr)
+library(anytime)
+library(readxl)
+library(ggridges)
+
+theme_set(theme_sleek())
+
+#COLOR BLIND PALETTE#
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
